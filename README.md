5176b0fe1a88919cedec91f827f2c8ebba19c098
Sean McGregor <smcgregor08@cmc.edu>
2013-03-21 13:33:45 -0700

    working demo

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ impress-video
 
 Video driver for the Impress.js presentation library. The intention of this project is to build a video driver for video APIs to synchronize their videos with user interactions in the [Impress.js](https://github.com/bartaz/impress.js/) presentation library.
 
-This driver is currently under development and is not ready for your attention. Vimeo has many eccentricities that require careful construction of slide transitions.
-
 # Specs #
+
+## Pluging in Your Video ##
+
+Edit the helloworld.html files in the demos folder and change the vimeo iframe URL to the video of your choice. Make sure you copy over the parameters found on the current video.
 
 ## Specifying Slide Timings ##
 
@@ -14,12 +16,3 @@ Slides are tied to the video by setting the following custom attributes:
 **data-video-seek: [seconds]**  
 Moves the time index of the current video to the time specified in the attribute value. The video should continue playing at the new time index unless it was already stopped. When the video reaches this slide, it will automatically change the viewed slides to match.
 
-## State Variables ##
-
-* currentVideoTime = [seconds]
-* videoStopped = [true|false]
-
-## Interactions with the Video ##
-
-* Users may stop the video, but the video will continue to seek to which slide they are viewing.
-* The video will automatically change the slide to the slide with the video's current time specified in the data-video-seek html attribute.
--- impress-video.js
+++ impress-video.js
@@ -1,8 +1,7 @@
 /**
  * @fileOverview
  * This file manages the interface between the JS presentation library
- * impress.js and an embedded video provider as defined in one of the 
- * []-interface.js scripts.
+ * impress.js and an embedded video provider Vimeo.
  *
  * Three events may cause the slides to change:
  * 1. The video is playing and hits the time for the next slide.
@@ -27,24 +26,211 @@
  * slide is valid.
  */
 var slideTimings = [];
-var slideApi = null;
-var currentVideoTime = 0;
+
+/**
+ * Stores the integer of the current slide number. This is used to index into
+ * the slideTimings array.
+ */
 var currentSlideNumber = null;
+
+/**
+ * Stores the integer of the next slide number. This is used to index into
+ * the slideTimings array.
+ */
 var nextNumber = null;
 
 /**
- * Must define several functions for the current video API.
- * This object should be initialized by one of the Interfaces
- * defined by the project.
+ * The impress.js API object.
+ * `api.init()` - initializes the presentation,
+ * `api.next()` - moves to next step of the presentation,
+ * `api.prev()` - moves to previous step of the presentation,
+ * `api.goto( idx | id | element, [duration] )` - moves the presentation to 
+ *       the step given by its index number id or the DOM element;
+ *       second parameter can be used to define duration of the transition in 
+ *       ms, but it's optional - if not provided default transition duration 
+ *       for the presentation will be used.
+ */
+var slideApi = null;
+
+/**
+ * Stores the vurrent state of the video's timer.
+ */
+var currentVideoTime = 0;
+
+/**
+ * Vimeo API handle.
+ */
+var froogaloop;
+
+/**
+ * URL of the video being played.
+ */
+var url;
+
+/**
+ * The video player object.
+ */
+var player;
+
+
+/**
+ * Handle messages received from the player.
+ * All messages are sent here and are forwarded 
+ * to the appropriate event handler.
+ */
+function onMessageReceived(e) {
+  
+  var data = JSON.parse(e.data);
+  
+  // If it is an API call the "method"
+  // will be defined, otherwise it is an
+  // event
+  if ( data.method !== undefined ) {
+    switch (data.method) {
+      case 'getCurrentTime':
+        onGetCurrentTime(data);
+        break;
+    }
+  } else {
+    switch (data.event) {
+      case 'ready':
+        onReady();
+        break;
+      case 'playProgress':
+        onPlayProgress(data.data);
+        break;
+      case 'loadProgress':
+        onLoadProgress(data.data);
+        break;
+      case 'pause':
+        onPause();
+        break;
+      case 'finish':
+        onFinish();
+        break;
+      case 'seek':
+        onSeek(data.data);
+        break;
+    }
+  }
+}
+
+/**
+ * Helper function for sending a message to the player.
  *
- * The expected functions are:
- * seek(seconds)
+ * @param {string} action the name of the Vimeo API action.
+ * @param {string} value the value of the action to send to the Vimeo API.
+ *                 This parameter is optional depending on the API call.
  *
  */
-var videoApi = {};
+function post(action, value) {
+  var data = { method: action };
+  
+  if (value) {
+    data.value = value;
+  }
+    
+  froogaloop[0].contentWindow.postMessage(JSON.stringify(data), url);
+}
+
+/**
+ * Register event listeners in the video player and start buffering immediatly
+ * by playing the video for a second, then pausing.
+ */
+function onReady() {
+  post('addEventListener', 'pause');
+  post('addEventListener', 'finish');
+  post('addEventListener', 'playProgress');
+  post('addEventListener', 'loadProgress');
+  post('addEventListener', 'seek');
+  
+  //Forces Vimeo to pre-load the video
+  post('play');
+  setTimeout(function(){
+    post('pause');
+  }, 1000);
+}
+
+/**
+ * Update the slide to the proper slide based on the current 
+ * time of the video player.
+ */
+function onGetCurrentTime(data) {
+  currentVideoTime = parseFloat(data.value);
+  
+  // Advance forward until reaching the correct time, or
+  // go back if it is too far forward
+  if(currentVideoTime > slideTimings[nextNumber]) {
+    while(currentVideoTime > slideTimings[nextNumber]) {
+      slideApi.next();
+      ++currentSlideNumber;
+      ++nextNumber;
+    }
+  } else if(currentVideoTime + 5 < slideTimings[currentSlideNumber]) {
+    // See: https://github.com/smcgregor/impress-video/issues/1
+    while(currentVideoTime < slideTimings[currentSlideNumber]) {
+      slideApi.prev();
+      --currentSlideNumber;
+      --nextNumber;
+    }
+  }
+}
+
+/**
+ * Used to ensure that the progress monitor loop is only entered once.
+ */
+var once = false;
+
+/**
+ * Will seek the video to the current slide when it is buffered, then it 
+ * starts the slide update loop.
+ */
+function onLoadProgress(data) {
+  
+  // Only complete this once
+  if ( once ) {
+    return;
+  }
+  
+  currentSlideNumber = parseInt($( ".active" ).attr("data-slide-number"));
+  nextNumber = currentSlideNumber + 1;
+  var timeLoaded = data.percent * data.duration;
+  
+  if ( timeLoaded > slideTimings[currentSlideNumber] ) {
+    once = true;
+    updateVideoBasedOnSlides();
+    loop();
+  }
+}
+
+//pass
+function onFinish() {}
+function onPlayProgress(data) {}
+function onSeek(data) {}
+function onPause() {}
+
+/**
+ * Function periodically checks the state of the video and 
+ * updates the slide show accordingly.
+ **/
+function loop() {
+  post("getCurrentTime");
+  setTimeout(loop, 500);
+}
+
+/**
+ * Sets the current slide and the next slide based on the slide that is
+ * currently in view. Then it seeks the video to match the displayed slide.
+ */
+function updateVideoBasedOnSlides() {
+  currentSlideNumber = parseInt($( ".active" ).attr("data-slide-number"));
+  nextNumber = currentSlideNumber + 1;
+  post("seekTo", slideTimings[currentSlideNumber]);
+}
 
 /**
  * Assigns the slideTimings array and numbers each of the slides.
+ * This should generally only be called on initialization.
  **/
 function updateSlideTimings() {
   slideTimings = [];
@@ -63,9 +249,51 @@ function updateSlideTimings() {
   });
 }
 
+// Supported keys are:
+// [space] - quite common in presentation software to move forward
+// [up] [right] / [down] [left] - again common and natural addition,
+// [pgdown] / [pgup] - often triggered by remote controllers,
+// [tab] - this one is quite controversial, see the impress.js source code
+document.addEventListener("keyup", function ( event ) {
+    if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+        switch( event.keyCode ) {
+            case 33: // pg up
+            case 37: // left
+            case 38: // up
+            case 9:  // tab
+            case 32: // space
+            case 34: // pg down
+            case 39: // right
+            case 40: // down
+                //delay a tenth of a second to allow the impress
+                //library to make the change
+                setTimeout(updateVideoBasedOnSlides, 100);
+                break;
+        }
+        event.preventDefault();
+    }
+}, false);
+
+
+// Call the API when a button is pressed.
+$('button').on('click', function() {
+    post($(this).text().toLowerCase());
+});
+
+// Setup everying after it is loaded
 $(window).load(function(){
- updateSlideTimings();
- impress().init();
- slideApi = impress();
- updateVideoBasedOnSlides();
+  
+  // Get Vimeo API handle
+  froogaloop = $('#player');
+  url = froogaloop.attr('src').split('?')[0];
+  
+  // Listen for messages from the player
+  if (window.addEventListener){
+    window.addEventListener('message', onMessageReceived, false);
+  } else {
+    window.attachEvent('onmessage', onMessageReceived, false);
+  }
+  updateSlideTimings();
+  impress().init();
+  slideApi = impress();
 });
